7e3cb07dd74cb902f1359bae3b66daab8f016889
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-09-09 19:03:10 +0500

    Task1

---
+++ Tyuiu.RagozinaAD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+     static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Алина";
+        int age = 17;
+        Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
