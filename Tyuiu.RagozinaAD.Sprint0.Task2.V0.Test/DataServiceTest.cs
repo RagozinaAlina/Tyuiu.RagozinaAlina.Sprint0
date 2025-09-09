c46a659857d8302bfab40562bf52e9d7ef52ebe2
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-09-09 20:14:03 +0500

    Task2

---
+++ Tyuiu.RagozinaAD.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.RagozinaAD.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.RagozinaAD.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Алина";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Алина", res);
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Tyuiu.RagozinaAD.Sprint0.Task2.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Алина"));
+        Console.ReadKey();
+    }
+}
